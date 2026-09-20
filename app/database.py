b3e5992ed4e2bd2673cf36eb4ac0caf3954e7b49
Luis Gamal Martinez Carbo <157326101+lmartinezcarbo@users.noreply.github.com>
2026-09-20 12:30:08 -0400

    refactor: simplify database configuration

--- app/database.py
+++ app/database.py
@@ -1,10 +1,8 @@
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.core.config import settings
 
-load_dotenv()
 
 DATABASE_URL = settings.database_url
 
